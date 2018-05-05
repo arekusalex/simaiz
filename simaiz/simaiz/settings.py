5e0a9b96109b6a3827b46fcf99d1d079adb71fb5
arekusalex <ed5leiva@gmail.com>
2018-05-05 00:26:19 -0600

    modificacion en settings.py para db

--- simaiz/simaiz/settings.py
+++ simaiz/simaiz/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'simaiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'simaiz_db',
+        'USER': 'miusuariodb',
+        'PASSWORD': 'miusuariodb',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
