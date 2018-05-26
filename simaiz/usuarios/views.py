a78fce042b33dc36be73b7b37ea85bddb9a61646
LLuucchhoo <lcortezdominguez@hotmail.com>
2018-05-26 13:07:03 -0600

    html de simulacion

--- simaiz/usuarios/views.py
+++ simaiz/usuarios/views.py
@@ -3,9 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import RegistrarForm
-
+from .forms import SimularForm
 class RegistrarUsuario(CreateView):
     model = User
     template_name = "usuarios/registrar.html"
     form_class = RegistrarForm
     success_url = reverse_lazy("main_inicio")
+
+class GenerarSimulacion(CreateView):
+	model=User
+	template_name="usuarios/generar_simulacion.html"
+	form_class=SimularForm
+	success_url=reverse_lazy("main_inicio")
