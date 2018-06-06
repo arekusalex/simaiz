3371595370083366e962a5b7a91777261763e97c
LLuucchhoo <lcortezdominguez@hotmail.com>
2018-06-06 16:05:19 -0600

    actualizar simulacion

--- simaiz/usuarios/views.py
+++ simaiz/usuarios/views.py
@@ -20,10 +20,21 @@ def generarSimulacion(request,id_sim):
     simulacion=Simulacion.objects.get(id=id_sim)
     aplicacion=Aplicacion.objects.filter(simulacion=simulacion)[0]
     area=aplicacion.terreno.area
-
+    cantidad=aplicacion.cantidad_fertilizante
+    fecha=aplicacion.fecha
+    fertilizante=aplicacion.fertilizante
+    Fosforo=aplicacion.fosforo_req
+    Potasio=aplicacion.potasio_req
+    Nitrogeno=aplicacion.nitrogeno_req
     context={
         'simulacion':simulacion,
         'area':area,
+        'cantidad':cantidad,
+        'fecha':fecha,
+        'fertilizante':fertilizante,
+        'nitrogeno':Nitrogeno,
+        'potasio':Potasio,
+        'fosforo':Fosforo,
     }
     return render(request,'usuarios/generar_simulacion.html',context)
 
@@ -44,40 +55,41 @@ class LineChartJSONView(BaseLineChartView):
                 [41, 92, 18, 3, 73, 87, 92],
                 ]
 class LineChartJSONView2(BaseLineChartView):
-    def get_labels1(self):
+    def get_labels(self):
         """Return 7 labels for the x-axis."""
+        return ["Fertika", "Surco", "Pradera"]
+
+    def get_providers(self):
+        """Return names of datasets."""
         return ["Potasio", "Fosforo", "Magnesio"]
 
-    def get_providers1(self):
-        """Return names of datasets."""
-        return ["Fertilizante1", "Fertilizante2"]
-
-    def get_data1(self):
+    def get_data(self):
         """Return 3 datasets to plot."""
 
         return [[75, 44, 92, 11, 44, 95, 35],
                 [41, 92, 18, 3, 73, 87, 92],
-                ]
+                [87, 21, 94, 3, 90, 13, 65]]
+
 class LineChartJSONView3(BaseLineChartView):
-    def get_labels3(self):
+    def get_labels(self):
         """Return 7 labels for the x-axis."""
-        return ["Potasio", "Fosforo", "Magnesio"]
+        return ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
 
-    def get_providers3(self):
+    def get_providers(self):
         """Return names of datasets."""
-        return ["Fertilizante1", "Fertilizante2"]
+        return ["LLuvioso", "Templado", "Despejado"]
 
-    def get_data3(self):
+    def get_data(self):
         """Return 3 datasets to plot."""
 
-        return [[75, 44, 92, 11, 44, 95, 35],
-                [41, 92, 18, 3, 73, 87, 92],
-                ]
+        return [[75, 44, 92, 11, 44, 95, 35, 75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92, 41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65, 87, 21, 94, 3, 90, 13, 65,]]
 
 line_chart = TemplateView.as_view(template_name='line_chart.html')
 line_chart_json = LineChartJSONView.as_view()
-line_chart1 = TemplateView.as_view(template_name='line_chart.html')
-line_chart_json1 = LineChartJSONView2.as_view()
-line_chart3 = TemplateView.as_view(template_name='line_chart.html')
+line_chart2 = TemplateView.as_view(template_name='line_chart2.html')
+line_chart_json2 = LineChartJSONView2.as_view()
+line_chart3 = TemplateView.as_view(template_name='line_chart3.html')
 line_chart_json3 = LineChartJSONView3.as_view()
 
