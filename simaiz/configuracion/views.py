b965609efa0de9a2edcc11d8c85f30dd97166df1
AP16014 <ap16014@ues.edu.sv>
2018-06-16 01:20:27 -0600

    implementando funcion editar y eliminar fertilizante en ventana modal

--- simaiz/configuracion/views.py
+++ simaiz/configuracion/views.py
@@ -11,10 +11,13 @@ from configuracion.forms import FertilizanteForm
 
 
 def configuracionSim(request):
+	listaFert = list()
+	hayConfi = False
 	if request.method == 'POST': #ya estan cargados los formularios
 		if 'btnForm1' in request.POST: #si se selecciona el boton de agregar precio
 			configuracion = Configuracion.objects.filter(usuario= request.user).exists()
 			if configuracion:
+				hayConfi = True
 				configuracion = Configuracion.objects.get(usuario= request.user)
 				form1 = ConfiguracionForm(request.POST, instance = configuracion)
 				form1.save()
@@ -23,6 +26,7 @@ def configuracionSim(request):
 				precio = request.POST.get('precio_maiz')
 				form1 = Configuracion(usuario=request.user, precio_maiz=precio, unidadMedidaMaiz = 10)
 				form1.save()
+				hayConfi = True
 				return redirect('configuracion')
 			
 
@@ -38,10 +42,24 @@ def configuracionSim(request):
 			form2.save()
 			return redirect('configuracion')
 
-	else:#al cargar la pagina
+		if 'inputEliminar' in request.POST:
+			idEliminar = request.POST.get('inputEliminar')
+			fertilizante = Fertilizante.objects.get(id =idEliminar)
+			fertilizante.delete()
+			return redirect('configuracion')
 
+		if 'btnEditarFert' in request.POST:
+			idEditar= request.POST.get('inputEditar')
+			fertilizante = Fertilizante.objects.get(id =idEditar)
+			formFertEdit = FertilizanteForm(request.POST, instance= fertilizante)
+			if formFertEdit.is_valid():
+				formFertEdit.save()
+			return redirect('configuracion')
+
+	else:#al cargar la pagina
 		configuracion = Configuracion.objects.filter(usuario= request.user).exists()
 		if configuracion:
+			hayConfi = True
 			configuracion = Configuracion.objects.get(usuario= request.user)
 			form1 = ConfiguracionForm(instance = configuracion)
 			form2 = FertilizanteForm
@@ -50,18 +68,29 @@ def configuracionSim(request):
 			form1 = ConfiguracionForm
 			form2 = FertilizanteForm
 
-
-	configuracion = Configuracion.objects.filter(usuario= request.user).exists()	
-	if configuracion:
-		configuracion = Configuracion.objects.get(usuario= request.user)
-		fertUsuarios = Fertilizante.objects.filter(configuracion=configuracion)
-	else:
-		fertUsuarios = ''
+		#Para cargar tabla
+		configuracion = Configuracion.objects.filter(usuario= request.user).exists()	
+		if configuracion:
+			hayConfi = True
+			configuracion = Configuracion.objects.get(usuario= request.user)
+			fertUsuarios = Fertilizante.objects.filter(configuracion=configuracion)
+			fForm = list()
+			for fer in fertUsuarios:
+				fertilizantesForm = FertilizanteForm(instance= fer)
+				fForm.append(fer)
+				fForm.append(fertilizantesForm)
+				listaFert.append(fForm)
+				fForm= []
+			#Para recuperar form de cada objeto
+		else:
+			fertUsuarios = ''
+	print(hayConfi)
 	contexto = {
 	'configuraciones':form1,
 	'fertilizantes':form2,
 	'fertUsuarios':fertUsuarios,
-	
+	'listaFert': listaFert,
+	'hayConfi': hayConfi,
 	
 	}
 	return render (request, 'configuracion/configuracion.html', {'contexto':contexto})
