115ed3497bdcfe9055997eb748469d11ae6ceaf0
rodrPM <32001094+rodrPM@users.noreply.github.com>
2018-06-18 02:20:48 -0600

    setiar fecha en crear/mod simulacion definitivo

--- simaiz/static/js/main/crear_simu.js
+++ simaiz/static/js/main/crear_simu.js
@@ -17,32 +17,25 @@ $(document).ready(function(){
 			$('.si_analisis').addClass('ocultar');
 		}
 	});
+	if($('#fecha_siembra').val()!=""){
+		cambiar_fecha();
+	}
 
-	// $('#fecha_siembra').change(function(){cambiar_fecha();});
-	$('#fecha_siembra').change(function(){
-		console.log('adentro')
-		fecha=new Date($('#fecha_siembra').val());
-		console.log();
-		f1=new Date();
-		f2=new Date();
-		f3=new Date();
-		idSem='#'+'s'+$('#select_sem').val();
-		diasCiclo=$(idSem).html();
-		f1.setDate(fecha.getDate()+diasCiclo*1);
-		f2.setDate(fecha.getDate()+diasCiclo*2);
-		f3.setDate(fecha.getDate()+diasCiclo*3);
-		$('#ap_1').text(f1.getDate()+'/'+(f1.getMonth()+1)+'/'+f1.getFullYear());
-		$('#ap_2').text(f2.getDate()+'/'+(f2.getMonth()+1)+'/'+f2.getFullYear());
-		$('#ap_3').text(f3.getDate()+'/'+(f3.getMonth()+1)+'/'+f3.getFullYear());
-	});
-	// $('#select_sem').change(cambiar_fecha());
+	$('#fecha_siembra').change(function(){cambiar_fecha();});
+	$('#select_sem').change(function(){cambiar_fecha();});
 });
 
 function cambiar_fecha(){
+	if($('#fecha_siembra').val()==""){
+		$('#ap_1').text('dd/mm/aaa');
+		$('#ap_2').text('dd/mm/aaa');
+		$('#ap_3').text('dd/mm/aaa');
+		return;
+	}
 	fecha=new Date($('#fecha_siembra').val());
-	f1=new Date();
-	f2=new Date();
-	f3=new Date();
+	f1=new Date(fecha);
+	f2=new Date(fecha);
+	f3=new Date(fecha);
 	idSem='#'+'s'+$('#select_sem').val();
 	diasCiclo=$(idSem).html();
 	f1.setDate(fecha.getDate()+diasCiclo*1);
